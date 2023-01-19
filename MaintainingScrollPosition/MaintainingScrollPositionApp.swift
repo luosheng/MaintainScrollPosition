478246e50a3617328991f2718a497886c05e3414
Sheng Luo <luosheng1986@gmail.com>
2023-01-19 14:24:38 +0800

    Initial Commit

---
+++ MaintainingScrollPosition/MaintainingScrollPositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaintainingScrollPositionApp.swift
+//  MaintainingScrollPosition
+//
+//  Created by Luo Sheng on 2023/1/19.
+//
+
+import SwiftUI
+
+@main
+struct MaintainingScrollPositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
