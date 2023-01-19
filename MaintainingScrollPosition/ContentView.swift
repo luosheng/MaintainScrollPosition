00b1eb280720eb373b3c42b12c6e31939092bfc1
Sheng Luo <luosheng1986@gmail.com>
2023-01-19 14:41:14 +0800

    Create a simple list

--- MaintainingScrollPosition/ContentView.swift
+++ MaintainingScrollPosition/ContentView.swift
@@ -8,19 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State var list: [Int]
+  @State var index = -1
+
+  init() {
+    var list: [Int] = []
+    for i in 0 ... 100 {
+      list.append(i)
     }
+    _list = State(initialValue: list)
+  }
+
+  var body: some View {
+    ScrollView {
+      LazyVStack {
+        ForEach(list, id: \.self) { i in
+          Text("\(i)")
+            .frame(height: 50)
+        }
+      }
+    }
+    .toolbar {
+      ToolbarItem {
+        Button {
+          list.insert(index, at: 0)
+          self.index = index - 1
+        } label: {
+          Image(systemName: "plus")
+        }
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
